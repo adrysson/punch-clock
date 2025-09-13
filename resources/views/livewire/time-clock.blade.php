4ace2ebb601bfc439ab93126ed16a32fbb191897
Adrysson <adryssonlc@gmail.com>
2025-09-13 17:46:49 -0300

    feat: melhora visualização da tabela de registro de ponto

--- resources/views/livewire/time-clock.blade.php
+++ resources/views/livewire/time-clock.blade.php
@@ -6,21 +6,31 @@
         <flux:button variant="primary" wire:click="punch">
             {{ __('Registrar ponto') }}
         </flux:button>
-        <div>
-            <table class="table-auto w-full mt-4">
-                <thead>
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th>{{ __('Data') }}</th>
-                        <th>{{ __('Hora') }}</th>
+                        <th scope="col" class="px-6 py-3">
+                            {{ __('Data') }}
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            {{ __('Hora') }}
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($timeClocks as $timeClock)
-                        <tr>
-                            <td>{{ $timeClock->created_at->format('d/m/Y') }}</td>
-                            <td>{{ $timeClock->created_at->format('H:i') }}</td>
-                        </tr>
-                    @endforeach
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        @foreach ($timeClocks as $timeClock)
+                            <tr>
+                                <td class="px-6 py-4">
+                                    {{ $timeClock->created_at->format('d/m/Y') }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $timeClock->created_at->format('H:i') }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tr>
                 </tbody>
             </table>
         </div>
